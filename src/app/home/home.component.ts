1a19f6b0f3f82dc16072a9be28470efe0def434a
edsont8 <edsont8@hotmail.com>
2021-07-18 01:41:15 -0300

    new layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+    let side1 = document.getElementById('side1');
+    let side2 = document.getElementById('side2');
+    let titulo1 = document.getElementById('titulo1');
+    let arrow = document.getElementById('arrow');
+
+    window.addEventListener('scroll', function(){
+      if(side1 != null && side2 != null && titulo1 != null && arrow != null) {
+         side1.style.left = -window.pageYOffset*1.5+'px';
+         side2.style.left = window.pageYOffset*1.5+'px';
+         titulo1.style.top = -window.pageYOffset*1.5+'px';
+         arrow.style.top = -window.pageYOffset*1.5+'px';
+       }
+
+    });
   }
 
 }
